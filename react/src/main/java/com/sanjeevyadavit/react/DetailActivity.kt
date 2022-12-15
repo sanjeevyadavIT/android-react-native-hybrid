169a247a37ee8dfe9e60564d938ad72b64190d68
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-12-15 21:00:25 +0530

    feat: Integrate React Native setup

--- react/src/main/java/com/sanjeevyadavit/react/DetailActivity.kt
+++ react/src/main/java/com/sanjeevyadavit/react/DetailActivity.kt
@@ -2,12 +2,76 @@ package com.sanjeevyadavit.react
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
 
-class DetailActivity : AppCompatActivity() {
+
+class DetailActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+
+    private var mReactRootView: ReactRootView? = null
+    private var mReactInstanceManager: ReactInstanceManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SoLoader.init(this, false);
+        mReactRootView = ReactRootView(this);
+        val packages: List<ReactPackage> = PackageList(application).getPackages()
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        // Remember to include them in `settings.gradle` and `app/build.gradle` too.
 
-        setContentView(R.layout.activity_detail)
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(getApplication())
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build();
+
+        // The string here (e.g. "MyReactNativeApp") has to match
+        // the string in AppRegistry.registerComponent() in index.js
+        mReactRootView!!.startReactApplication(mReactInstanceManager, "reactNative", null);
+
+        setContentView(mReactRootView);
+//        setContentView(R.layout.activity_detail)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostPause(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostResume(this, this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostDestroy(this)
+        }
+        if (mReactRootView != null) {
+            mReactRootView!!.unmountReactApplication()
+        }
     }
 }
