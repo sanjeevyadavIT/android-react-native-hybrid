0e129890416c1d3e7193526bb49d64bcbda727b8
Sanjeev Yadav <sanjeevy133@protonmail.com>
2022-12-19 12:25:41 +0530

    feat: Upgrade to React Native 0.69.7

--- react/src/main/java/com/sanjeevyadavit/react/CustomReactDelegate.kt
+++ react/src/main/java/com/sanjeevyadavit/react/CustomReactDelegate.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import com.facebook.react.ReactDelegate
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactRootView
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 
 class CustomReactDelegate(
@@ -17,6 +18,14 @@ class CustomReactDelegate(
     appKey,
     launchOptions
 ), DefaultHardwareBackBtnHandler {
+
+    override fun createRootView(): ReactRootView {
+        val reactRootView = ReactRootView(activity)
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED)
+        return reactRootView
+    }
 
     override fun onHostResume() {
         if (reactNativeHost.hasInstance()) {
@@ -35,4 +44,10 @@ class CustomReactDelegate(
     override fun invokeDefaultOnBackPressed() {
 //        (activity as DefaultHardwareBackHandler).invokeDefaultOnBackPressed()
     }
+
+//    override fun isConcurrentRootEnabled(): Boolean {
+//        // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+//        // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+//        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+//    }
 }
--- react/src/main/java/com/sanjeevyadavit/react/ReactNativeModule.kt
+++ react/src/main/java/com/sanjeevyadavit/react/ReactNativeModule.kt
@@ -1,9 +1,19 @@
 package com.sanjeevyadavit.react
 
 import android.app.Application
+import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.ReactPackageTurboModuleManagerDelegate
+import com.facebook.react.bridge.*
+import com.facebook.react.fabric.ComponentFactory
+import com.facebook.react.fabric.CoreComponentsRegistry
+import com.facebook.react.fabric.FabricJSIModuleProvider
+import com.facebook.react.fabric.ReactNativeConfig
 import com.facebook.react.shell.MainReactPackage
+import com.facebook.react.uimanager.ViewManagerRegistry
+import com.sanjeevyadavit.react.newarchitecture.components.MainComponentsRegistry
+import com.sanjeevyadavit.react.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,21 +27,110 @@ class ReactNativeModule {
     @Provides
     @Singleton
     fun provideReactNativeHost(application: Application): ReactNativeHost {
-        return object : ReactNativeHost(application) {
-            override fun getUseDeveloperSupport(): Boolean {
-                return BuildConfig.DEBUG
-            }
 
-            override fun getPackages(): List<ReactPackage> {
-                return listOf<ReactPackage>(
-                    MainReactPackage()
-                )
-            }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            /**
+             * A {@link ReactNativeHost} that helps you load everything needed for the New Architecture, both
+             * TurboModule delegates and the Fabric Renderer.
+             *
+             * <p>Please note that this class is used ONLY if you opt-in for the New Architecture (see the
+             * `newArchEnabled` property). Is ignored otherwise.
+             */
+            return object : ReactNativeHost(application) {
+                override fun getUseDeveloperSupport(): Boolean {
+                    return BuildConfig.DEBUG
+                }
 
-            override fun getJSMainModuleName(): String {
-                return "index"
-            }
+                override fun getPackages(): List<ReactPackage?>? {
+                    val packages: MutableList<ReactPackage> = mutableListOf()
 
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    //     packages.add(new MyReactNativePackage());
+                    // TurboModules must also be loaded here providing a valid TurboReactPackage implementation:
+                    //     packages.add(new TurboReactPackage() { ... });
+                    // If you have custom Fabric Components, their ViewManagers should also be loaded here
+                    // inside a ReactPackage.
+                    packages.add(MainReactPackage())
+
+                    return packages
+                }
+
+                override fun getReactPackageTurboModuleManagerDelegateBuilder(): ReactPackageTurboModuleManagerDelegate.Builder {
+                    // Here we provide the ReactPackageTurboModuleManagerDelegate Builder. This is necessary
+                    // for the new architecture and to use TurboModules correctly.
+                    return MainApplicationTurboModuleManagerDelegate.Builder()
+                }
+
+                override fun getJSMainModuleName(): String? {
+                    return "index"
+                }
+
+                override fun getJSIModulePackage(): JSIModulePackage? {
+                    return object : JSIModulePackage {
+                        override fun getJSIModules(
+                            reactApplicationContext: ReactApplicationContext,
+                            jsContext: JavaScriptContextHolder
+                        ): List<JSIModuleSpec<UIManager>> {
+                            val specs: MutableList<JSIModuleSpec<UIManager>> = mutableListOf()
+
+                            // Here we provide a new JSIModuleSpec that will be responsible of providing the
+                            // custom Fabric Components.
+                            specs.add(
+                                object : JSIModuleSpec<UIManager> {
+                                    override fun getJSIModuleType(): JSIModuleType {
+                                        return JSIModuleType.UIManager
+                                    }
+
+                                    override fun getJSIModuleProvider(): JSIModuleProvider<UIManager> {
+                                        // Here we register a Components Registry.
+                                        // The one that is generated with the template contains no components
+                                        // and just provides you the one from React Native core.
+                                        val componentFactory = ComponentFactory()
+                                        CoreComponentsRegistry.register(componentFactory)
+
+                                        // Here we register a Components Registry.
+                                        // The one that is generated with the template contains no components
+                                        // and just provides you the one from React Native core.
+                                        MainComponentsRegistry.register(componentFactory)
+                                        val reactInstanceManager: ReactInstanceManager =
+                                            getReactInstanceManager()
+                                        val viewManagerRegistry = ViewManagerRegistry(
+                                            reactInstanceManager.getOrCreateViewManagers(
+                                                reactApplicationContext
+                                            )
+                                        )
+                                        return FabricJSIModuleProvider(
+                                            reactApplicationContext,
+                                            componentFactory,
+                                            ReactNativeConfig.DEFAULT_CONFIG,
+                                            viewManagerRegistry
+                                        )
+                                    }
+                                })
+                            return specs
+                        }
+                    }
+                }
+
+            }
+        } else {
+            return object : ReactNativeHost(application) {
+                override fun getUseDeveloperSupport(): Boolean {
+                    return BuildConfig.DEBUG
+                }
+
+                override fun getPackages(): List<ReactPackage> {
+                    return listOf<ReactPackage>(
+                        MainReactPackage()
+                    )
+                }
+
+                override fun getJSMainModuleName(): String {
+                    return "index"
+                }
+
+            }
         }
+
     }
 }
